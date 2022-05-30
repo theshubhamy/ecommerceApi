58eab070c41366ff557da372032dcbc0a389367d
shubham kumar <devshubhamyadav@gmail.com>
2022-05-30 16:04:33 +0530

    complete

--- controllers/authentication/admin/admin-login-email.js
+++ controllers/authentication/admin/admin-login-email.js
@@ -6,35 +6,48 @@ import bcrypt from "bcryptjs";
 import User from "../../../models/user.js";
 
 //helpers
-import {validationErrorHandler} from "../../../helpers/validation-error-handler.js";
-
+import { validationErrorHandler } from "../../../helpers/validation-error-handler.js";
 
 export const adminLoginEmail = async (req, res, next) => {
   validationErrorHandler(req, next);
-  const {email, password} = req.body;
+  const { email, password } = req.body;
   try {
-    const admin = await User.findOne({where: {email, isAdmin: true}});
+    const admin = await User.findOne({ where: { email, isAdmin: true } });
     if (!admin) {
-      const error = new Error('Admin not found');
+      const error = new Error("Admin not found");
       error.statusCode = 404;
       return next(error);
     }
-    const isPwdEqual = await bcrypt.compare(password, admin["dataValues"]["password"]);
+    const isPwdEqual = await bcrypt.compare(
+      password,
+      admin["dataValues"]["password"]
+    );
     if (!isPwdEqual) {
-      const error = new Error('Wrong Password');
+      const error = new Error("Wrong Password");
       error.statusCode = 401;
       return next(error);
     }
     const id = admin["dataValues"]["id"];
     const name = admin["dataValues"]["name"];
     const phone = admin["dataValues"]["phone"];
-    const token = jwt.sign({id, phone}, process.env.TOKEN_SIGNING_KEY, {expiresIn: '1 day'});
-    const refreshToken = jwt.sign({id, phone, name}, process.env.REFRESH_TOKEN_SIGNING_KEY);
-    await User.update({refreshToken: refreshToken}, {where: {email, phone}});
+    const token = jwt.sign({ id, phone }, process.env.TOKEN_SIGNING_KEY, {
+      expiresIn: "1 day",
+    });
+    const refreshToken = jwt.sign(
+      { id, phone, name },
+      process.env.REFRESH_TOKEN_SIGNING_KEY
+    );
+    await User.update(
+      { refreshToken: refreshToken },
+      { where: { email, phone } }
+    );
     res.status(201).json({
       msg: `Login with email Successful`,
+      name: name,
+      email: email,
+      phone: phone,
       token: token,
-      refreshToken: refreshToken
+      refreshToken: refreshToken,
     });
   } catch (err) {
     if (!err.statusCode) {
--- controllers/authentication/admin/admin-login-phone.js
+++ controllers/authentication/admin/admin-login-phone.js
@@ -5,28 +5,35 @@ import jwt from "jsonwebtoken";
 import User from "../../../models/user.js";
 
 //helpers
-import {validationErrorHandler} from "../../../helpers/validation-error-handler.js";
-
+import { validationErrorHandler } from "../../../helpers/validation-error-handler.js";
 
 export const adminLoginPhone = async (req, res, next) => {
   validationErrorHandler(req, next);
-  const {phone, otp} = req.body;
+  const { phone, otp } = req.body;
   try {
-    const admin = await User.findOne({where: {phone, otp, isAdmin: true}});
+    const admin = await User.findOne({ where: { phone, otp, isAdmin: true } });
     if (!admin) {
-      const error = new Error('Admin not found');
+      const error = new Error("Admin not found");
       error.statusCode = 404;
       return next(error);
     }
     const id = admin["dataValues"]["id"];
     const name = admin["dataValues"]["name"];
-    const token = jwt.sign({id, phone}, process.env.TOKEN_SIGNING_KEY, {expiresIn: '1 day'});
-    const refreshToken = jwt.sign({id, phone, name}, process.env.REFRESH_TOKEN_SIGNING_KEY);
-    await User.update({isVerified: true, refreshToken: refreshToken, otp: null}, {where: {phone}});
+    const token = jwt.sign({ id, phone }, process.env.TOKEN_SIGNING_KEY, {
+      expiresIn: "1 day",
+    });
+    const refreshToken = jwt.sign(
+      { id, phone, name },
+      process.env.REFRESH_TOKEN_SIGNING_KEY
+    );
+    await User.update(
+      { isVerified: true, refreshToken: refreshToken, otp: null },
+      { where: { phone } }
+    );
     res.status(201).json({
       msg: `Phone number ${phone} verified successfully`,
       token: token,
-      refreshToken: refreshToken
+      refreshToken: refreshToken,
     });
   } catch (err) {
     if (!err.statusCode) {
