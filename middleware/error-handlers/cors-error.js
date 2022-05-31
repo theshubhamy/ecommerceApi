9e68d03c901d0219b192d33d7c4f700672ae79bd
shubham kumar <devshubhamyadav@gmail.com>
2022-05-31 16:58:53 +0530

    complete

--- middleware/error-handlers/cors-error.js
+++ middleware/error-handlers/cors-error.js
@@ -1,5 +1,5 @@
 export const corsError = (req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:3000");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, PATCH, DELETE"
