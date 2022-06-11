d2eff20a73fb8264b55adc6c4549859b79835420
shubham kumar <devshubhamyadav@gmail.com>
2022-06-11 10:52:18 +0530

    newdeb setup

--- utilities/database.js
+++ utilities/database.js
@@ -1,26 +1,3 @@
-// import Sequelize from "sequelize";
-
-// const sequelize = new Sequelize(
-//   "ecommerceapi",
-//   "root",
-//   "",
-//   {
-//     dialect: "mysql",
-//     host: "localhost",
-//     logging: false,
-//   },
-//   {
-//     pool: {
-//       max: 100,
-//       min: 0,
-//       acquire: 30000,
-//       idle: 10000,
-//     },
-//   }
-// );
-
-// export default sequelize;
-//production
 import fs from "fs";
 import Sequelize from "sequelize";
 import {
