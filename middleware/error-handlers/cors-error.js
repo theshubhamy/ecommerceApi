a0a7755b2675aa2af15036ebb19c5a914f168ba4
shubham kumar <devshubhamyadav@gmail.com>
2022-07-03 14:07:02 +0530

    product details

--- middleware/error-handlers/cors-error.js
+++ middleware/error-handlers/cors-error.js
@@ -1,5 +1,8 @@
 export const corsError = (req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "http://localhost:3000,https://client-apnamart.vercel.app, https://admin-apnamart.vercel.app"
+  );
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, PATCH, DELETE"
