bbdbd1c4200d66533df71cac4b469639eab0ec67
shubham kumar <devshubhamyadav@gmail.com>
2022-07-03 15:46:57 +0530

    product details

--- utilities/database.js
+++ utilities/database.js
@@ -42,27 +42,4 @@ if (env === "DEV") {
 }
 
 const sequelize = new Sequelize(sequelizeOptions);
-
 export default sequelize;
-// import Sequelize from "sequelize";
-
-// const sequelize = new Sequelize(
-//   "ecommercedb",
-//   "root",
-//   "",
-//   {
-//     dialect: "mysql",
-//     host: "localhost",
-//     logging: false,
-//   },
-//   {
-//     pool: {
-//       max: 100,
-//       min: 0,
-//       acquire: 30000,
-//       idle: 10000,
-//     },
-//   }
-// );
-
-// export default sequelize;
