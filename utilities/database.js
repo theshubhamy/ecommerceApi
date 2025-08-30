34e1ef5a991d923117490913c241bf0d27ed6f7a
Shubham Kumar <devshubhamyadav@gmail.com>
2025-08-30 20:18:47 +0530

    Update database.js

--- utilities/database.js
+++ utilities/database.js
@@ -1,22 +1,17 @@
 import Sequelize from "sequelize";
+import { username, password, host, port, database } from "./constants.js";
 
-const sequelize = new Sequelize(
-  "ecommercedb",
-  "root",
-  "",
-  {
-    dialect: "mysql",
-    host: "localhost",
-    logging: false,
+const sequelize = new Sequelize(database, username, password, {
+  dialect: "mysql",
+  host,
+  port,
+  logging: false,
+  pool: {
+    max: 100,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
   },
-  {
-    pool: {
-      max: 100,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-  }
-);
+});
 
 export default sequelize;
